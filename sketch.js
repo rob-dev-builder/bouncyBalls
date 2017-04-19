231b63f97b576d89e16cf576fcf74851cd32b197
Rob <superserva@gmail.com>
2017-04-19 21:25:20 +1200

    add style and text

--- sketch.js
+++ sketch.js
@@ -1,8 +1,5 @@
 var balls = [];
 
-var bounceSound = document.getElementsByTagName("audio")[0];
-// document.getElementsByTagName("audio")[0].play();
-
 function mousePressed() {
     for (var i = 0; i < balls.length; i++) {
         if (balls[i].clicked() == true) {
@@ -13,63 +10,27 @@ function mousePressed() {
     }
 }
 
-
 function keyPressed() {
   if (keyCode === UP_ARROW) {
     //value = 255;
     // add new ball if up arrow is pressed
-    balls.push(new Ball())
-
+    balls.push(new Ball());
   } else if (keyCode === DOWN_ARROW) {
-    //value = 0;
+    // remove a ball if down arraow is pressed
     balls.splice(0,1);
   }
 }
 
-// function collision_Dectection() {
-//     for (var i = 0; i < balls.length; i++) {
-//         for (var j = 0; j < balls.length; j++) {
-//
-//             if (balls[i] == balls[j]// if comparing a ball with itself, do nothing
-//
-//             ) {} else if (balls[i] != balls[j]) {
-//                 var d = dist(balls[i].x, balls[i].y, balls[j].x, balls[j].y);
-//
-//                 //line(balls[i].x, balls[i].y, balls[j].x, balls[j].y);
-//                 // Draw line between balls
-//
-//                 var ballradius = balls[i].radius + balls[j].radius;
-//                 var trueDist = d - ballradius;
-//                 console.log("distance from edges = " + trueDist);
-//
-//                 if (trueDist <= 2) {
-//                     //console.log('Collision detected')
-//                     bounceSound.play();
-//                     balls[i].xSpeed = balls[i].xSpeed * -1;
-//                     balls[i].ySpeed = balls[i].ySpeed * -1;
-//
-//                     balls[j].xSpeed = balls[j].xSpeed * -1;
-//                     balls[j].ySpeed = balls[j].ySpeed * -1;
-//                 } else if (trueDist >= 3) {}
-//
-//             }
-//
-//         }
-//
-//     }
-//
-// }
-
 function setup() {
     // frameRate(0.9);
-    createCanvas(400, 400);
+    createCanvas(800, 600);
     for (var i = 0; i < 3; i++) {
         balls[i] = new Ball();
     }
 }
 
 function draw() {
-    background(255, 255, 30);
+    background(30, 30, 30);
     for (var i = 0; i < balls.length; i++) {
         balls[i].displayBall();
         balls[i].moveBall();
