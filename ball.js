a471d83867177e3e9e244c1c5a80b3bdeb510384
Rob <superserva@gmail.com>
2017-04-19 16:52:10 +1200

    add up arrow adds a ball

--- ball.js
+++ ball.js
@@ -7,59 +7,58 @@ function Ball() { // Constructor
     this.ySpeed = random(-2, 2); // Speed
 
     // select random location on screen to place ball
-     this.x = random(this.diameter, (width - this.diameter));
-     this.y = random(this.diameter, (height - this.diameter));
+    this.x = random(this.diameter, (width - this.diameter));
+    this.y = random(this.diameter, (height - this.diameter));
 
     // Set a random colour for ball
     this.colourR = random(0, 255);
     this.colourG = random(0, 255);
     this.colourB = random(0, 255);
 
-
-this.changeColour = function() {
-    this.colourR = random(0, 255);
-    this.colourG = random(0, 255);
-    this.colourB = random(0, 255);
-}
-
-this.clicked = function() {
-
-    var d = dist(this.x, this.y, mouseX, mouseY);
-
-    if (d < this.radius) { // return true if mouse cursor inside ball
-        return true;
-    } else if (d > this.radius) { // return false of mouse cursor is not inside ball
-        return false;
+    this.changeColour = function() {
+        this.colourR = random(0, 255);
+        this.colourG = random(0, 255);
+        this.colourB = random(0, 255);
     }
-}
 
-this.displayBall = function() {
-    fill(this.colourR, this.colourB, this.colourG);
-    ellipse(this.x, this.y, this.diameter, this.diameter);
-}
+    this.clicked = function() {
 
-this.moveBall = function() {
-    this.x = this.x + this.xSpeed;
-    this.y = this.y + this.ySpeed;
-}
+        var d = dist(this.x, this.y, mouseX, mouseY);
 
-this.checkEdge = function() {
+        if (d < this.radius) { // return true if mouse cursor inside ball
+            return true;
+        } else if (d > this.radius) { // return false of mouse cursor is not inside ball
+            return false;
+        }
+    }
 
-    // if ball edge hits left or right side of box
-    if (((this.x + this.radius) > width) || ((this.x - this.radius) < 0)) {
-        // invert the number for x direction. This sends ball in opisite direction.
-        this.xSpeed = this.xSpeed * -1;
-        this.changeColour();
+    this.displayBall = function() {
+        fill(this.colourR, this.colourB, this.colourG);
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
 
-    } else {}
+    this.moveBall = function() {
+        this.x = this.x + this.xSpeed;
+        this.y = this.y + this.ySpeed;
+    }
 
-    // if ball edge hits top or bottom of box
-    if (((this.y + this.radius) > height) || ((this.y - this.radius) < 0)) {
-        // invert the number for x direction. This sends ball in opisite direction.
-        this.ySpeed = this.ySpeed * -1;
-        this.changeColour();
+    this.checkEdge = function() {
 
-    } else {}
-}
+        // if ball edge hits left or right side of box
+        if (((this.x + this.radius) > width) || ((this.x - this.radius) < 0)) {
+            // invert the number for x direction. This sends ball in opisite direction.
+            this.xSpeed = this.xSpeed * -1;
+            this.changeColour();
+
+        } else {}
+
+        // if ball edge hits top or bottom of box
+        if (((this.y + this.radius) > height) || ((this.y - this.radius) < 0)) {
+            // invert the number for x direction. This sends ball in opisite direction.
+            this.ySpeed = this.ySpeed * -1;
+            this.changeColour();
+
+        } else {}
+    }
 
 }
--- sketch.js
+++ sketch.js
@@ -6,20 +6,29 @@ function mousePressed() {
             //balls.splice(i, 1)
             balls[i].xSpeed = balls[i].xSpeed * -1;
 
-        } else if (balls[i].clicked() == false) {
-
-        }
+        } else if (balls[i].clicked() == false) {}
     }
+}
+
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    //value = 255;
+    // add new ball if up arrow is pressed
+    balls.push(new Ball())
+
+  } else if (keyCode === DOWN_ARROW) {
+    //value = 0;
+  }
 }
 
 function collision_Dectection() {
     for (var i = 0; i < balls.length; i++) {
         for (var j = 0; j < balls.length; j++) {
 
-            if (balls[i] == balls[j]) {}
-            // if comparing a ball with itself, do nothing
+            if (balls[i] == balls[j]// if comparing a ball with itself, do nothing
 
-            else if (balls[i] != balls[j]) {
+            ) {} else if (balls[i] != balls[j]) {
                 var d = dist(balls[i].x, balls[i].y, balls[j].x, balls[j].y);
 
                 //line(balls[i].x, balls[i].y, balls[j].x, balls[j].y);
@@ -29,19 +38,14 @@ function collision_Dectection() {
                 var trueDist = d - ballradius;
                 console.log("distance from edges = " + trueDist);
 
-                if (trueDist <= 2)
-                {
-                   //console.log('Collision detected')
+                if (trueDist <= 2) {
+                    //console.log('Collision detected')
                     balls[i].xSpeed = balls[i].xSpeed * -1;
                     balls[i].ySpeed = balls[i].ySpeed * -1;
 
                     balls[j].xSpeed = balls[j].xSpeed * -1;
                     balls[j].ySpeed = balls[j].ySpeed * -1;
-                }
-                else if(trueDist >= 3)
-                {
-
-                }
+                } else if (trueDist >= 3) {}
 
             }
 
@@ -52,7 +56,7 @@ function collision_Dectection() {
 }
 
 function setup() {
-  //  frameRate(0.9);
+    //  frameRate(0.9);
     createCanvas(400, 400);
     for (var i = 0; i < 3; i++) {
         balls[i] = new Ball();
